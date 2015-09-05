7f660933fd8e418070502c04dbefcf2211b96c02
Gr8 <gr8_boi52@yahoo.com>
2015-09-04 18:58:36 -0700

    added new SQL queries

--- SQL_SCRIPTS/server_1.sql
+++ SQL_SCRIPTS/server_1.sql
@@ -1,11 +1,32 @@
 /* =================== DELETES =================== */
 
+/* DELETE Dead Players */
 DELETE from player
 WHERE is_alive = 0;
 
-/* =================== UDPATES =================== */
+/* DELETE Bugged Players */
+DELETE FROM player 
+WHERE is_alive = 1 
+and damage = 1;
 
-UPDATE construction 
-SET is_locked = -1
-WHERE pin_code <> '000000' 
-AND is_locked = 0;
+DELETE FROM player 
+WHERE account_uid = 'SomethingWentWrong'
+OR account_uid = '';
+
+/* DELETE old Players */
+DELETE FROM player
+WHERE LastUpdated < NOW() - INTERVAL 14 DAY;
+
+/* DELETE old accounts */
+DELETE FROM account
+WHERE last_connect_at < NOW() - INTERVAL 30 DAY;
+
+/* DELETE old Vehicles */
+DELETE FROM vehicle
+WHERE LastUpdated < NOW() - INTERVAL 7 DAY;
+
+/* DELETE old Containers */
+DELETE FROM container
+WHERE last_accessed < NOW() - INTERVAL 14 DAY;
+
+/* =================== UDPATES =================== */
