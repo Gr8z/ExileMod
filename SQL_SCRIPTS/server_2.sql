2908c6e0e4876193dfb404440eb87a001b8f364f
Gr8 <gr8_boi52@yahoo.com>
2015-09-12 16:37:55 -0700

    fixed mySQL maintainace

--- SQL_SCRIPTS/server_2.sql
+++ SQL_SCRIPTS/server_2.sql
@@ -1,20 +1,20 @@
 /* =================== DELETES =================== */
 
 /* DELETE Dead Players */
-DELETE from player
+DELETE from player_2
 WHERE is_alive = 0;
 
 /* DELETE Bugged Players */
-DELETE FROM player 
+DELETE FROM player_2
 WHERE is_alive = 1 
 and damage = 1;
 
-DELETE FROM player 
+DELETE FROM player_2
 WHERE account_uid = 'SomethingWentWrong'
 OR account_uid = '';
 
 /* DELETE old Players */
-DELETE FROM player
+DELETE FROM player_2
 WHERE LastUpdated < NOW() - INTERVAL 14 DAY;
 
 /* DELETE old accounts */
@@ -22,11 +22,11 @@ DELETE FROM account
 WHERE last_connect_at < NOW() - INTERVAL 30 DAY;
 
 /* DELETE old Vehicles */
-DELETE FROM vehicle
+DELETE FROM vehicle_2
 WHERE LastUpdated < NOW() - INTERVAL 7 DAY;
 
 /* DELETE old Containers */
-DELETE FROM container
+DELETE FROM container_2
 WHERE last_accessed < NOW() - INTERVAL 14 DAY;
 
 /* =================== UDPATES =================== */
