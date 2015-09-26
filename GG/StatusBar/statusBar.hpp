e32217a198437aae73064f9a8f72fc7f3c4005df
Gr8 <gr8_boi52@yahoo.com>
2015-09-26 13:41:40 -0700

    Icon Status Bar

--- GG/StatusBar/statusBar.hpp
+++ GG/StatusBar/statusBar.hpp
@@ -1,10 +1,10 @@
 #define ST_RIGHT 0x01
-
-class statusBar {
+ 
+class osefStatusBarAdmin {
 	idd = -1;
-	onLoad = "uiNamespace setVariable ['statusBar', _this select 0]";
-	onUnload = "uiNamespace setVariable ['statusBar', objNull]";
-	onDestroy = "uiNamespace setVariable ['statusBar', objNull]";
+	onLoad = "uiNamespace setVariable ['osefStatusBarAdmin', _this select 0]";
+	onUnload = "uiNamespace setVariable ['osefStatusBarAdmin', objNull]";
+	onDestroy = "uiNamespace setVariable ['osefStatusBarAdmin', objNull]";
 	fadein = 0;
 	fadeout = 0;
 	duration = 10e10;
@@ -13,22 +13,55 @@ class statusBar {
 	objects[] = {};
 	class controls {
 		class statusBarText {
-			idc = 1000;
-			x = safezoneX + safezoneW - 1.4;
-			y = safezoneY + safezoneH - 0.04;
-			w = 1;
-			h = 0.04;
+			idc = 55554;
+			x = safezoneX + safezoneW - 1.90;
+			y = safezoneY + safezoneH - 0.063;
+			w = 1.35;
+			h = 0.06;
 			shadow = 2;
-			colorBackground[] = { 1, 0.3, 0, 0.0 };
+//			colorBackground[] = { 0, 0, 0, 0.5 };  // uncomment and increase 4th number to have a background
 			font = "PuristaSemibold";
-			size = 0.03;
+			size = 0.04;
 			type = 13;
-			style = 1;
-			text="loading...";
+			style = 2;
+			text="";
 			class Attributes {
-				align="left";
-				color = "#FFFFFF";
+				align="center";
+				color = "#ffffff";//#5fe60c
 			};
 		};
 	};
-};
+}; 
+
+class osefStatusBar {
+	idd = -1;
+	onLoad = "uiNamespace setVariable ['osefStatusBar', _this select 0]";
+	onUnload = "uiNamespace setVariable ['osefStatusBar', objNull]";
+	onDestroy = "uiNamespace setVariable ['osefStatusBar', objNull]";
+	fadein = 0;
+	fadeout = 0;
+	duration = 10e10;
+	movingEnable = 0;
+	controlsBackground[] = {};
+	objects[] = {};
+	class controls {
+		class statusBarText {
+			idc = 55555;
+			x = safezoneX + safezoneW - 1.50;
+			y = safezoneY + safezoneH - 0.035;
+			w = 1.3;
+			h = 0.07;
+			shadow = 2;
+//			colorBackground[] = { 0, 0, 0, 0.5 };  // uncomment and increase 4th number to have a background
+			font = "PuristaSemibold";
+			size = 0.04;
+			type = 13;
+			style = 2;
+			text="";
+			class Attributes {
+				align="center";
+				color = "#ffffff";//#5fe60c
+			};
+		};
+	};
+}; 
