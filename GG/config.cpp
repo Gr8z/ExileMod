b4e8c2fe600267fdff9e82501b82272fdc534c7a
Gr8 <gr8_boi52@yahoo.com>
2015-10-03 17:56:16 -0700

    no speed Limit

--- GG/config.cpp
+++ GG/config.cpp
@@ -1565,6 +1565,7 @@ class CfgExileCustomCode
 	ExileClient_object_player_event_onEnterSafezone = "GG\safezones\GG_object_player_event_onEnterSafezone.sqf";
 	ExileClient_object_player_event_onLeaveSafezone = "GG\safezones\GG_object_player_event_onLeaveSafezone.sqf";
 	ExileClient_gui_inventory_event_onSlotDoubleClick = "GG\overrides\GG_gui_inventory_event_onSlotDoubleClick.sqf";
+	ExileClient_gui_safezone_safeESP = "GG\overrides\GG_gui_safezone_safeESP.sqf";
 };
 class CfgExileMusic
 {
