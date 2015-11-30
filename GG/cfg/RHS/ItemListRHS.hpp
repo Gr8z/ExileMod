4027241c7e49ae42aa6c189cde10b04613519120
GhostGamerz <Psychotic_closure@hotmail.com>
2015-11-30 11:59:45 -0500

    derps

--- GG/cfg/RHS/ItemListRHS.hpp
+++ GG/cfg/RHS/ItemListRHS.hpp
@@ -386,7 +386,7 @@
 	///
 	//Btr's
 	///
-	class rhs_btr60_chdkz	 						{ quality = 1; price = 20000; };
+	class rhs_btr60_chdkz	 						{ quality = 1; price = 45000; };
 	
 	///////////////////////////////////////////////////////////////////////////////
 	// URAL
@@ -398,8 +398,8 @@
 	class RHS_Ural_Fuel_MSV_01						{ quality = 1; price = 3000; };
 	class RHS_Ural_Open_MSV_01						{ quality = 1; price = 3000; };
 	class rhs_Ural_Flat_MSV_01 						{ quality = 1; price = 2500; };
-	class rhsusf_M108A1P2_B_M2_d_fmtv_usarmy 		{ quality = 1; price = 40000; };
-	class rhsusf_M1083A1P2_B_M2_d_open_fmtv_usarmy 	{ quality = 1; price = 34200; };
+	class rhsusf_M108A1P2_B_M2_d_fmtv_usarmy 		{ quality = 1; price = 30000; };
+	class rhsusf_M1083A1P2_B_M2_d_open_fmtv_usarmy 	{ quality = 1; price = 25000; };
 
 	///////////////////////////////////////////////////////////////////////////////
 	// CH-47
