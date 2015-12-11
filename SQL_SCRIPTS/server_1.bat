788377049b3e2a2ba40db55aa1435e4f1b0e5d24
Gr8 <gr8_boi52@yahoo.com>
2015-12-10 19:07:34 -0800

    fixed DB name

--- SQL_SCRIPTS/server_1.bat
+++ SQL_SCRIPTS/server_1.bat
@@ -1,17 +1,11 @@
 @echo off
 @REM *** PARAMETERS/VARIABLES ***
-SET mysqlschema=exile_1
+SET mysqlschema=exile
 SET mysqlpassword=03333755478
 SET mysqluser=root
 @REM *** EXECUTION ***
 
 echo.
-echo.
-echo RUNNING PHP FIX Script...
-echo.
-echo.
-C:\xampp\php\php.exe C:\xampp\htdocs\database\server_1\fix_construction_protection.php
-C:\xampp\php\php.exe C:\xampp\htdocs\database\server_1\delete_abandoned_vehicles.php
 echo.
 echo.
 echo RUNNING SQL Cleanup Script...
