0c41122ebd081be8d3c34655c298228ac43e8d29
shahzad1900 <shazzy1900@gmail.com>
2016-02-07 04:28:35 +0000

    Revert "added orca"
    This reverts commit e1454ce2325937e4c0dbef936be584cef349a791.

--- GG/config.cpp
+++ GG/config.cpp
@@ -1784,7 +1784,7 @@ class CfgExileArsenal
 	class Exile_Chopper_Orca_CSAT								{ quality = 1; price = 12000; };
 	class Exile_Chopper_Orca_Black								{ quality = 1; price = 12000; };
 	class Exile_Chopper_Orca_BlackCustom						{ quality = 1; price = 12000; };
-	class D_heli_light_02_v2_F									{ quality = 1; price = 420000; };
+	class O_heli_light_02_v2_F									{ quality = 1; price = 220000; };
 
 	///////////////////////////////////////////////////////////////////////////////
 	// Taru
