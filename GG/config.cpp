084607cb1a399f2ddedda818efda802a7f1cc7b6
Gr8 <gr8_boi52@yahoo.com>
2016-02-25 16:45:06 -0800

    zombies config

--- GG/config.cpp
+++ GG/config.cpp
@@ -8,7 +8,22 @@
  * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License. 
  * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/.
  */
- 
+
+class CfgTornZ_Exile
+{
+    zombies_maxLocal = 12;
+    zombies_maxLocalOverFlow = 24;
+    zombies_maxTotal = 40;
+    zombies_distance = 150;
+    zombies_distanceMax = 250;
+    zombies_distanceLosInital = 100;
+    zombies_distanceLosMax = 200;
+    zombies_distanceLosInital_EngineOff = 20;
+    zombies_spawnChance = 0.7;
+    zombies_spawnOutsideChance = 0.3;
+    zombies_lootChance = 0.8;
+    zombies_killedScore = 50;
+}; 
 
 class CfgClans
 {
