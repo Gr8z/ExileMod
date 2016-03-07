52ec82b3a8d815db1fee7b5d4bc130c3c16faa70
Gr8 <gr8_boi52@yahoo.com>
2016-03-06 16:28:13 -0800

    notifications

--- GG/config.cpp
+++ GG/config.cpp
@@ -25,6 +25,65 @@ class CfgTornZ_Exile
     zombies_killedScore = 10;
 }; 
 
+class CfgNotifications
+{
+  class Success
+  {
+      description = "%1";
+      title = "Success";
+      iconPicture = "\A3\ui_f\data\map\mapcontrol\taskIcon_ca.paa";
+      iconText = "";
+      color[] = {0.7, 0.93, 0, 1};
+      duration = 5;
+      priority = 0;
+      difficulty[] = {};
+  };
+  class Whoops
+  {
+      description = "%1";
+      title = "Error";
+      iconPicture = "\A3\ui_f\data\map\mapcontrol\taskIcon_ca.paa";
+      iconText = "";
+      color[] = {0.7, 0.93, 0, 1};
+      duration = 5;
+      priority = 0;
+      difficulty[] = {};
+  };
+	class ZCP_Init
+	{
+			description = "%1";
+			title = "Battle Zone Spawned";
+			iconPicture = "\A3\ui_f\data\map\mapcontrol\taskIcon_ca.paa";
+			iconText = "";
+			color[] = {0.7, 0.93, 0, 1};
+			duration = 5;
+			priority = 0;
+			difficulty[] = {};
+	};
+	class ZCP_Capped
+	{
+		description = "%1";
+		title = "Battle Zone Capped";
+		iconPicture = "\A3\ui_f\data\map\mapcontrol\taskIcon_ca.paa";
+		iconText = "";
+		color[] = {0.7, 0.93, 0, 1};
+		duration = 5;
+		priority = 0;
+		difficulty[] = {};
+	};
+	class ZCP_Capping
+	{
+		description = "%1";
+		title = "Battle Zone alert";
+		iconPicture = "\A3\ui_f\data\map\mapcontrol\taskIcon_ca.paa";
+		iconText = "";
+		color[] = {0.7, 0.93, 0, 1};
+		duration = 5;
+		priority = 0;
+		difficulty[] = {};
+	};
+};
+
 class CfgClans
 {
 	/*
