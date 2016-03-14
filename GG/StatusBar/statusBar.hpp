bf183e3bfdc144129c69e92b00437c7983dfbfdf
Gr8 <gr8_boi52@yahoo.com>
2016-03-14 14:42:13 -0700

    fixed status bar position

--- GG/StatusBar/statusBar.hpp
+++ GG/StatusBar/statusBar.hpp
@@ -1,29 +1,30 @@
 class RscExileStatusBar
 {
         idd = -1;
-		duration = 10e10;
+        duration = 10e10;
         onLoad = "uiNamespace setVariable ['RscExileStatusBar', _this select 0];";
-		fadein = 0;
-		fadeout = 0;
-		movingEnable = 0;
-		objects[] = {};
-
+        fadein = 0;
+        fadeout = 0;
+        movingEnable = 0;
+        objects[] = {};
+ 
         class controlsBackground
         {
                 class statusBarImage
                 {
                         idc = 55557;
-						type = 0;
-						style = 48;
-                        x = safezoneX + safezoneW - 1.883;
+                        type = 0;
+                        style = 48;
+                        x = safezoneX + safezoneW - 1.5;
                         y = safezoneY + safezoneH - 0.092; //0.068
-                        w = 1.35;
+                        w = 1;
                         h = 0.08; //0.06
                         colorText[] = {1, 1, 1, 1};
-						colorBackground[]={0,0,0,0};
-						sizeEx = 0.4;
-						font = "OrbitronLight";
+                        colorBackground[]={0,0,0,0};
+                        sizeEx = 0.4;
+                        font = "OrbitronLight";
                         text = "GG\images\StatusBarBackground.paa";
+                        align="center";
                 };
         };
  
@@ -32,17 +33,17 @@ class RscExileStatusBar
                 class statusBarText
                 {
                         idc = 55554;
-                        x = safezoneX + safezoneW - 1.90;
+                        x = safezoneX + safezoneW - 1.5;
                         y = safezoneY + safezoneH - 0.063;
-                        w = 1.35;
+                        w = 1;
                         h = 0.06;
                         shadow = 2;
                         font = "OrbitronLight";
                         size = 0.032;
                         type = 13;
-						style = 2;
+                        style = 2;
                         text = "";
- 
+                        align="center";
                         class Attributes
                         {
                                 align="center";
