d70fcc34d6f61fa8c8ab9db7b909bf20e6a4cd01
Gr8 <gr8_boi52@yahoo.com>
2016-03-14 19:53:43 -0700

    more statusbar align fixes

--- GG/StatusBar/statusBar.hpp
+++ GG/StatusBar/statusBar.hpp
@@ -15,16 +15,21 @@ class RscExileStatusBar
                         idc = 55557;
                         type = 0;
                         style = 48;
-                        x = safezoneX + safezoneW - 1.5;
-                        y = safezoneY + safezoneH - 0.092; //0.068
-                        w = 1;
-                        h = 0.08; //0.06
+                        x = safezoneX + 0.3 * safezoneW;
+                        y = safezoneY + 0.946 * safezoneH;
+                        w = safezoneW * 0.42 ;
+                        h = safezoneH * 0.05;
                         colorText[] = {1, 1, 1, 1};
                         colorBackground[]={0,0,0,0};
                         sizeEx = 0.4;
                         font = "OrbitronLight";
                         text = "GG\images\StatusBarBackground.paa";
-                        align="center";
+                        class Attributes
+                        {
+                                align="center";
+                                color = "#ffffff";
+                                font = "OrbitronLight";
+                        };
                 };
         };
  
@@ -33,13 +38,13 @@ class RscExileStatusBar
                 class statusBarText
                 {
                         idc = 55554;
-                        x = safezoneX + safezoneW - 1.5;
-                        y = safezoneY + safezoneH - 0.063;
-                        w = 1;
-                        h = 0.06;
+                        x = safezoneX + 0 * safezoneW;
+                        y = safezoneY + 0.965 * safezoneH;
+                        w = safezoneW * 1;
+                        h = safezoneH * 1;
                         shadow = 2;
                         font = "OrbitronLight";
-                        size = 0.032;
+                        size = 0.03;
                         type = 13;
                         style = 2;
                         text = "";
