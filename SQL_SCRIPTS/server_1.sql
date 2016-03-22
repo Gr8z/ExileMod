8efbd17c3dbe8c9df97b240251922afa18d55035
Gr8 <gr8_boi52@yahoo.com>
2016-03-22 12:33:41 -0700

    fixed last update mysql

--- SQL_SCRIPTS/server_1.sql
+++ SQL_SCRIPTS/server_1.sql
@@ -47,4 +47,4 @@ and damage = 1;
 /* RESET VEHICLE CODE NOT USED IN 5 DAYS */
 UPDATE vehicle
 SET pin_code = 0000,is_locked = 0
-WHERE LastUpdated < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 3 DAY);
+WHERE last_updated_at < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 3 DAY);
