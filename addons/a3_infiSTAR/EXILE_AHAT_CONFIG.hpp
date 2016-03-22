91395b49b7a34e39f9329677dbcad810fc6ee1a3
GhostGamerz <Psychotic_closure@hotmail.com>
2016-03-22 12:51:32 -0400

    Revert "nasty denied"
    This reverts commit 14779e2a77936b36b0f83830114d5e9577f71063.

--- addons/a3_infiSTAR/EXILE_AHAT_CONFIG.hpp
+++ addons/a3_infiSTAR/EXILE_AHAT_CONFIG.hpp
@@ -129,6 +129,7 @@ adminUIDandAccess[] =
 		 "76561198065188130", // Nacho
 		 "76561198074948727", // Sifi_link
 		 "76561198073495490", // Adam Kadmon
+		 "76561198056028211", // Nasty
 		 // Add UID's Above - Don't forget the comma!
 		 "0"},	/* Admins with UIDs in this Array have their Access defined in the array below. */
 		{
