417cb55f661993c03c3eb6e32719f041e3b63e9a
Gr8 <gr8_boi52@yahoo.com>
2016-03-27 16:08:41 -0700

    rcon pass changed

--- addons/a3_infiSTAR/EXILE_AHAT_CONFIG.hpp
+++ addons/a3_infiSTAR/EXILE_AHAT_CONFIG.hpp
@@ -7,7 +7,7 @@ serverCommandPassword = "penis1234";
 	
 	This is used to be able to use the "login" function ingame. Will log you in as Arma "Admin".
 */
-passwordAdmin = "GG1029";
+passwordAdmin = "MOJOMOJO";
 /* serverCommandPassword and passwordAdmin should always be different passwords or it could cause problems! */
 
 
