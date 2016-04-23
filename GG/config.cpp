05c756b087e8e05a59f28eb6e4efb2ad0d3aae21
Gr8 <gr8_boi52@yahoo.com>
2016-04-23 10:16:22 -0700

    Panthera Temperature settings

--- GG/config.cpp
+++ GG/config.cpp
@@ -2491,43 +2491,99 @@ class CfgExileEnvironment
 		};
 	};
 
-	class Namalsk: Altis 
+	class Panthera3
 	{
-		class FireFlies: FireFlies
+		class FireFlies
 		{
+			// 1 = enabled, 0 = disabled
 			enable = 1;
+
+			// At this hour fire flies begin to spawn
+			startHour = 18;
+
+			// At this hour fire flies stop spawning
+			endHour = 4;
 		};
 
-		class Anomalies: Anomalies
+		class Anomalies
 		{
-			enable = 1;
+			// 1 = enabled, 0 = disabled
+			enable = 0;
+
+			// At this hour anomalies begin to spawn
+			startHour = 19;
+
+			// At this hour anomalies stop spawning
+			endHour = 6;
 		};
 
-		class Breathing: Breathing
+		class Breathing
 		{
-			enable = 1;
+			// 1 = enabled, 0 = disabled
+			enable = 0;
 		};
 
-		class Snow: Snow
+		class Snow
 		{
-			enable = 1;
-			surfaces[] = {"#nam_snow"};
+			// 1 = enabled, 0 = disabled
+			enable = 0;
+
+			// https://community.bistudio.com/wiki/surfaceType
+			surfaces[] = {};
 		};
 
-		class Radiation: Radiation
+		class Radiation
 		{
-			enable = 1;
-			contaminatedZones[] = 
-			{
-				{{3960.14,	8454.75,	152.862}, 	80, 	140},	// Object A1
-				{{4974.70,	6632.82,	4.74293}, 	40, 	150},	// Object A2
-				{{6487.92,	9302.03,	36.0014}, 	60, 	110}	// Sebjan Chemical Factory
-			};
+			// 1 = enabled, 0 = disabled
+			enable = 0;
+
+			/*
+				Defines contaminated zones in a specific map.
+				You can define multiple zones per map. The format
+				of the zones is:
+
+				[Position ASL(!), Full Radiation Radius, Maximum Radius]
+
+				The radius works as follows:
+
+	            |-------------------------------------------------------|
+	                                Maximum Radius
+
+				|------------------------|
+				  Full Radiation Radius
+
+				Within the full radiation radius, radiation factor is
+				always at a maximum. Outside of this, it lowers down
+				to no radiation smoothly.
+
+				Radiation:
+
+	            |------------------------|------------------------------|
+	            1        1      1        1     0.75    0.5     0.25    0
+			*/
+			contaminatedZones[] = {};
 		};
 
-		class Temperature: Temperature
+		class Temperature
 		{
-			daytimeTemperature[] = {-2.00,-1.77,-1.12,-0.10,1.24,2.78,4.40,6.00,7.46,8.65,9.50,9.90,9.90,9.50,8.65,7.46,6.00,4.40,2.78,1.24,-0.10,-1.12,-1.77,-2.00,-2.00};
+			// Temperature in °C for the time of day, per hour
+			// Add the first index to the last index, so it is 25 indizes!
+			daytimeTemperature[] = {15.93,16.89,18.42,20.40,22.68,25.10,27.48,29.63,31.40,32.66,33.32,33.80,33.80,33.32,32.66,31.40,29.63,27.48,25.10,22.68,20.40,18.42,16.89,15.93,15.93};
+
+			// Temperature change in °C when it is 100% overcast
+			overcast = -2;
+
+			// Temperature change in °C when it is 100% raining
+			rain = -5;
+
+			// Temperature change in °C when it is 100% windy
+			wind = -1;
+
+			// Temperature change per 100m altitude in °C
+			altitude = -0.5;
+
+			// Difference from the daytime temperature to the water temperature
+			water = -5;
 		};
 	};
 };
