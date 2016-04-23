e8363d54a0425ae9a80a63531176144ade849fdb
Gr8 <gr8_boi52@yahoo.com>
2016-04-23 08:54:47 -0700

    panthera config

---
+++ SQL_SCRIPTS/server_3.bat
@@ -0,0 +1,15 @@
+@echo off
+@REM *** PARAMETERS/VARIABLES ***
+SET mysqlschema=exile_3
+SET mysqlpassword=madgr8
+SET mysqluser=root
+@REM *** EXECUTION ***
+
+echo.
+echo.
+echo.
+echo RUNNING SQL Cleanup Script...
+echo.
+echo.
+D:\MySQL\bin\mysql.exe -u %mysqluser% -p%mysqlpassword% %mysqlschema%  < "C:\ExileMod\SQL_SCRIPTS\server_3.sql"
+exit
