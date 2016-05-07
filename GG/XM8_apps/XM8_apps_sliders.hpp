e40809d48646ae573ef0b93b9bba9ce4b63ca694
Gr8 <gr8_boi52@yahoo.com>
2016-05-07 14:58:57 -0700

    Revert "full paths ?"
    This reverts commit 8c31189058dfa9fb7d7e5008fdd230bc6bfb15f2.

--- GG/XM8_apps/XM8_apps_sliders.hpp
+++ GG/XM8_apps/XM8_apps_sliders.hpp
@@ -9,24 +9,24 @@ class CfgXM8 {
 	class craftSlide {
 		controlID = 105000;
 		title = "Crafting";
-		onLoadScript = "GG\XM8_apps\apps\XM8_crafting\XM8_crafting_craftSlide_onLoad.sqf";
+		onLoadScript = "XM8_crafting_craftSlide_onLoad";
 	};
 	//This slide use IDCs from 106000 to 
 	class territorySlide {
 		controlID = 106000;
 		title = "Territory";
-		onLoadScript = "GG\XM8_apps\apps\XM8_territory\XM8_territory_territorySlide_onLoad.sqf";
+		onLoadScript = "XM8_territory_territorySlide_onLoad";
 	};
 	//This slide use IDCs from 107000 to 
 	class airDropSlide {
 		controlID = 107000;
 		title = "Air drop";
-		onLoadScript = "GG\XM8_apps\apps\XM8_airDrop\XM8_airDrop_airDropSlide_onLoad.sqf";
+		onLoadScript = "XM8_airDrop_airDropSlide_onLoad";
 	};
 	//This slide use IDCs from 108000 to 
 	class playerStatsSlide {
 		controlID = 108000;
 		title = "Player stats";
-		onLoadScript = "GG\XM8_apps\apps\XM8_playerStats\XM8_playerStats_playerStatsSlide_onLoad.sqf";
+		onLoadScript = "XM8_playerStats_playerStatsSlide_onLoad";
 	};
 };
