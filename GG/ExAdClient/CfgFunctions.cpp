59b8028f0c999fc5578c0a7c536ca8ab7b63e898
Gr8 <gr8_boi52@yahoo.com>
2016-05-13 21:57:12 -0700

    comment out hacking

--- GG/ExAdClient/CfgFunctions.cpp
+++ GG/ExAdClient/CfgFunctions.cpp
@@ -23,7 +23,7 @@ class ExAd
 	#include "Core\CfgFunctions.cpp"
 	#include "VirtualGarage\CfgFunctions.cpp"
 	//#include "AdminEvents\CfgFunctions.cpp"
-	#include "Hacking\CfgFunctions.cpp"
+	//#include "Hacking\CfgFunctions.cpp"
 	//#include "Grinding\CfgFunctions.cpp"
 	#include "HaloParachute\CfgFunctions.cpp"	
 };
