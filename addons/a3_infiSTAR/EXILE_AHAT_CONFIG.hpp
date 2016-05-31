b3a2ed7d2c0176542d188df5635891f5d3afe2c5
GhostGamerz <Psychotic_closure@hotmail.com>
2016-05-31 15:04:49 -0700

    *Correction Dexter*

--- addons/a3_infiSTAR/EXILE_AHAT_CONFIG.hpp
+++ addons/a3_infiSTAR/EXILE_AHAT_CONFIG.hpp
@@ -151,6 +151,7 @@ adminUIDandAccess[] =
 		 "76561198129318411", // FriendlyFire
 		 "76561198274767971", // CanadianFrodo
 		 "76561198065188130", // Nacho
+		 "76561198036004541", // Demen
 		 "0"},	/* Admins with UIDs in this Array have their Access defined in the array below. */
 		{
 			"Teleport On Map Click","Teleport - Target To Me","Teleport - Me To Target","Teleport In Facing Direction (10m steps)",
@@ -176,7 +177,6 @@ adminUIDandAccess[] =
 	},
 	{
 		{
-		 "76561198056745227", // Dexter
 		 "76561198060389676", // Powerwolf
 		 "76561198062993387", // Azul
 		 "76561197987069300", // Shroombear
