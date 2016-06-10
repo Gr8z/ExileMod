917f00c2ce22b5c484e8cc2cbd2260f1fa68c24c
Nasty <elmythproductions@gmail.com>
2016-06-10 17:00:57 +0200

    Fixed category names, removed spaces that bugged items out, added missing letters...tl;dr fixed shit.

--- GG/cfg/RHS/TraderCategoriesRHS.hpp
+++ GG/cfg/RHS/TraderCategoriesRHS.hpp
@@ -61,7 +61,7 @@
 			"rhs_6b26",
 			"rhs_6b26_ess",
 			"rhs_6b26_bala_green",
-			"rhs_6b27m ",
+			"rhs_6b27m",
 			"rhs_6b27m_ml",
 			"rhs_6b27m_green_ess",
 			"rhs_6b28_flora",
@@ -177,6 +177,30 @@
 		};
 	};
 	
+	class RHSShotguns
+	{
+		name = "RHS Shotguns";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[]=
+		{
+			"rhs_weap_M590_5RD",
+			"rhs_weap_M590_8RD"
+		};
+	};
+	
+	class RHSSubMachineGuns
+	{
+		name = "RHS Sub Machine Guns";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[]=
+		{
+			"rhsusf_weap_MP7A1_base_f",
+			"rhsusf_weap_MP7A1_aor1",
+			"rhsusf_weap_MP7A1_desert",
+			"rhsusf_weap_MP7A1_winter"
+		};
+	};
+	
 	class RHSLightMachineGuns
 	{
 		name = "RHS Light Machine Guns";
@@ -203,8 +227,10 @@
 		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
 		items[]=
 		{
-			"rhs_weap_M590_5RD",
-			"rhs_weap_M590_8RD",
+			"rhs_weap_hk416d10",
+			"rhs_weap_hk416d10_m320",
+			"rhs_weap_hk416d145",
+			"rhs_weap_hk416d145_m320",
 			"rhs_weap_m16a4",
 			"rhs_weap_m16a4_carryhandle",
 			"rhs_weap_m16a4_carryhandle_grip",
@@ -350,7 +376,7 @@
 		};
 	};
 	
-	class RHSVehicles
+		class RHSVehicles
 	{
 		name = "RHS Vehicles";
 		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
@@ -371,8 +397,6 @@
 			"rhs_tigr_m_3camo_vdv",
 			"rhsusf_rg33_d",
 			"rhsusf_rg33_wd",
-			"rhsusf_rg33_m2_d",
-			"rhsusf_rg33_m2_wd",
 			"rhsusf_m998_w_2dr_fulltop",
 			"rhsusf_m998_d_2dr_fulltop",
 			"RHS_UAZ_MSV_01",
@@ -381,6 +405,18 @@
 			"RHS_Ural_Flat_MSV_01",
 			"RHS_Ural_MSV_01",
 			"RHS_Ural_Open_Civ_03",
+			"rhsusf_m113_usarmy_unarmed"
+		};
+	};
+	
+	class RHSArmedVehicles
+	{
+		name = "RHS Vehicles";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[]=
+		{
+			"rhsusf_rg33_m2_d",
+			"rhsusf_rg33_m2_wd",
 			"rhsusf_m1025_w_m2",
 			"rhsusf_m1025_d_m2",
 			"rhsusf_M1078A1P2_B_M2_wd_fmtv_usarmy",
@@ -390,7 +426,6 @@
 			"rhsusf_m113_usarmy",
 			"rhsusf_M977A4_BKIT_M2_usarmy_wd",
 			"rhsusf_m113_usarmy_M240",
-			"rhsusf_m113_usarmy_unarmed",
 			"rhs_btr60_chdkz",
 			"rhs_btr70_msv"
 		};
@@ -419,9 +454,9 @@
 		};
 	};
 	
-	class RHSMines
+	class RHSExplosives
 	{
-		name = "RHS Mines";
+		name = "RHS Explosives";
 		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\cargothrow_ca.paa";
 		items[]=
 		{
