3d0efe79e682966b9a675f2b81409cf2e3e27837
Nasty <elmythproductions@gmail.com>
2016-06-21 19:22:42 +0200

    Mas Categories / Prices
    Will be subject to changes.

---
+++ GG/cfg/MAS/TraderCategoriesMAS.hpp
@@ -0,0 +1,509 @@
+	class MASPointerAttachments 
+	{
+		name = "MAS Pointer Attachments";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] = 
+		{
+	"acc_mas_flash_gun", 
+	"acc_mas_pointer_gun_IR", 
+	"acc_mas_pointer_IR", 
+	"acc_mas_pointer_IR_b", 
+	"acc_mas_pointer_IR_top", 
+	"acc_mas_pointer_IR_top_b", 
+	"acc_mas_pointer_IR2", 
+	"acc_mas_pointer_IR2_top", 
+	"acc_mas_pointer_IR2c", 
+	"acc_mas_pointer_IR2c_top"
+		};
+	};
+
+	class MASMuzzleAttachments 
+	{
+		name = "MAS Suppressor Attachments";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemmuzzle_ca.paa";
+		items[] = 
+		{
+	"muzzle_mas_snds_L", 
+	"muzzle_mas_snds_LM", 
+	"muzzle_mas_snds_C", 
+	"muzzle_mas_snds_MP5SD6", 
+	"muzzle_mas_snds_M", 
+	"muzzle_mas_snds_Mc", 
+	"muzzle_mas_snds_MP7", 
+	"muzzle_mas_snds_AK", 
+	"muzzle_mas_snds_SM", 
+	"muzzle_mas_snds_SMc", 
+	"muzzle_mas_snds_SH", 
+	"muzzle_mas_snds_SHc", 
+	"muzzle_mas_snds_SVD" 
+		};
+	};
+
+	class MASOpticAttachments 
+	{
+		name = "MAS Scopes";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemoptic_ca.paa";
+		items[] = 
+		{
+	"optic_mas_DMS", 
+	"optic_mas_DMS_c", 
+	"optic_mas_Holosight_blk", 
+	"optic_mas_Holosight_camo", 
+	"optic_mas_Arco_blk", 
+	"optic_mas_Arco_camo", 
+	"optic_mas_Hamr_camo", 
+	"optic_mas_Aco_camo", 
+	"optic_mas_ACO_grn_camo", 
+	"optic_mas_MRCO_camo", 
+	"optic_mas_zeiss", 
+	"optic_mas_zeiss_c", 
+	"optic_mas_zeiss_eo", 
+	"optic_mas_zeiss_eo_c", 
+	"optic_mas_acog", 
+	"optic_mas_acog_c", 
+	"optic_mas_acog_eo", 
+	"optic_mas_acog_eo_c", 
+	"optic_mas_acog_rd", 
+	"optic_mas_acog_rd_c", 
+	"optic_mas_handle", 
+	"optic_mas_aim", 
+	"optic_mas_aim_c", 
+	"optic_mas_PSO", 
+	"optic_mas_PSO_c", 
+	"optic_mas_PSO_eo", 
+	"optic_mas_PSO_eo_c", 
+	"optic_mas_PSO_nv", 
+	"optic_mas_PSO_nv_c", 
+	"optic_mas_PSO_nv_eo", 
+	"optic_mas_PSO_nv_eo_c", 
+	"optic_mas_PSO_day", 
+	"optic_mas_PSO_nv_day", 
+	"optic_mas_term", 
+	"optic_mas_MRD", 
+	"optic_mas_LRPS", 
+	"optic_mas_kobra", 
+	"optic_mas_kobra_c", 
+	"optic_mas_nspu", 
+	"optic_mas_goshawk", 
+	"optic_mas_PSO_kv", 
+	"optic_mas_PSO_kv_c"	
+		};
+	};
+
+	class MASAmmunition
+	{
+		name = "MAS Ammunition";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] = 
+		{	
+	//Hk 416 ammo
+	"30Rnd_mas_556x45_Stanag",
+	"30Rnd_mas_556x45sd_Stanag", 	//SD
+	"30Rnd_mas_556x45_T_Stanag", 	//tracer
+	//Hk 417 ammo
+	"20Rnd_mas_762x51_Stanag",
+	"20Rnd_mas_762x51sd_Stanag",	//SD
+	"20Rnd_mas_762x51_T_Stanag", 	//tracer
+	//M27 IAR ammo
+	"150Rnd_mas_556x45_Stanag", 	//Drum
+	"150Rnd_mas_556x45_T_Stanag", 	//Drum tracer
+	//SCAR-H ammo
+	"20Rnd_mas_762x51_Stanag",
+	"20Rnd_mas_762x51sd_Stanag", 	//SD
+	"20Rnd_mas_762x51_T_Stanag", 	//tracer
+	//MP-40
+	"30Rnd_mas_9x21_Stanag",
+	//Lee-Enfield No4
+	"5Rnd_mas_762x51_Stanag",
+	"5Rnd_mas_762x51_T_Stanag", 	//tracer
+	//Benelli M1014 (shotgun) shells
+	"7Rnd_mas_12Gauge_Slug", 		//slug
+	"7Rnd_mas_12Gauge_Pellets", 	//Pellets
+	//AA-12
+	"20Rnd_mas_12Guage_Slug", 		//slug
+	"20Rnd_mas_12Guage_Pellets", 	//pellets
+	//M107 ammo
+	"5Rnd_mas_127x99_Stanag",
+	"5Rnd_mas_127x99_T_Stanag",		//tracer
+	//M24 ammo
+	"5Rnd_mas_762x51sd_Stanag", 	//SD
+	//LRR AWM ammo
+	"10Rnd_mas_338_Stanag",
+	"10Rnd_mas_338sd_Stanag", 		//SD
+	"10Rnd_mas_338_T_Stanag",		//tracer
+	//MP5
+	"30Rnd_mas_9x21_Stanag",
+	"30Rnd_mas_9x21sd_Stanag",		//SD
+	"30Rnd_mas_9x21d_Stanag",		//underwater
+	//MP7A1 ammo
+	"40Rnd_mas_46x30_Mag",
+	"40Rnd_mas_46x30sd_Mag",		//SD
+	//Stoner LMG (mk200) ammo
+	"200Rnd_mas_556x45_Stanag",
+	"200Rnd_mas_556x45_T_Stanag",	//tracer
+	//Mk48/M240/MG3/M60 ammo
+	"100Rnd_mas_762x51_Stanag",
+	"100Rnd_mas_762x51_T_Stanag",	//tracer
+	//PPSh-41 ammo
+	"71Rnd_mas_762x25_mag",
+	//AK74M/AKS74/AKS74U/AK74/AK12/AKMS/AKM/Zastava M70 ammo
+	"30Rnd_mas_545x39_mag",
+	"30Rnd_mas_545x39sd_mag",		//SD
+	"30Rnd_mas_545x39_T_mag",		//tracer
+	//Bizon ammo
+	"64Rnd_mas_9x18_mag",
+	"64Rnd_mas_9x18sd_mag",			//SD
+	//Saiga 12 ammo
+	"10Rnd_mas_12Gauge_Slug",
+	"10Rnd_mas_12Gauge_Pellets",
+	//Zastava M91 ammo
+	"10Rnd_mas_762x54_mag",
+	"10Rnd_mas_762x54_T_mag",		//tracer
+	//Zastava M72 ammo
+	"100Rnd_mas_762x39_mag",
+	"100Rnd_mas_762x39_T_mag",		//tracer
+	//SVD ammo
+	"10Rnd_mas_762x54sd_mag",
+	//VSS Vintorez ammo
+	"20Rnd_mas_9x39_mag",
+	"20Rnd_mas_9x39sd_mag",			//SD
+	//AS VAL ammo
+	"30Rnd_mas_9x39_mag",
+	"30Rnd_mas_9x39sd_mag",			//SD
+	"30Rnd_mas_9x39d_mag",			//underwater
+	//KSVK ammo
+	"5Rnd_mas_127x108_mag",
+	//"5Rnd_mas_127x108_dem_mag",	//explosive
+	"5Rnd_mas_127x108_T_mag",		//tracer
+	//RPK ammo
+	"100Rnd_mas_545x39_mag",
+	"100Rnd_mas_545x39_T_mag",		//tracer
+	//PKM/PKP ammo
+	"100Rnd_mas_762x54_mag",
+	"100Rnd_mas_762x54_T_mag",		//tracer
+	//Micro Uzi
+	"25Rnd_mas_9x19_Mag",
+	//SA61 Skorpion ammo
+	"20Rnd_mas_765x17_Mag",
+	//M9/Browning HP/Glock 17/Sig p226 ammo
+	"17Rnd_mas_9x21_Mag",
+	"15Rnd_mas_9x21_Mag",
+	"13Rnd_mas_9x21_Mag",
+	//MP-443 Grach/Makarov ammo
+	"8Rnd_mas_9x18_Mag"
+		};
+	};
+
+	class MASPistols 
+	{
+		name = "MAS Pistols";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\handgun_ca.paa";
+		items[] = 
+		{
+	//Makarov
+	"hgun_mas_mak_F"			// stock
+	//Micro UZI
+	"hgun_mas_uzi_F",			// stock
+	//M9
+	"hgun_mas_m9_F",			// stock
+	//Browning HP
+	"hgun_mas_bhp_F",			// stock
+	//Glock 17
+	"hgun_mas_glock_F",			// stock
+	//Sig p226
+	"hgun_mas_p226_F",			// stock
+	//MP-443 Grach
+	"hgun_mas_grach_F",			// stock
+	//SA61 Skorpion
+	"hgun_mas_sa61_F",			// stock
+	//MP7A1  PISTOL SLOT
+	"hgun_mas_mp7p_F"			// stock
+		};
+	};
+	
+		class MASShotguns
+	{
+		name = "MAS Shotguns";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] = 
+		{
+	//Saiga 12
+	"arifle_mas_saiga",			// stock
+	//Benelli M1014 (shotgun)
+	"arifle_mas_m1014", 		// stock
+	//AA-12
+	"arifle_mas_aa12" 			// stock
+		};
+	};
+
+	class MASSubMachineGuns 
+	{
+		name = "MAS Sub Machine Guns";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] = 
+		{
+	//MP-40
+	"arifle_mas_mp40", 			// closed stock
+	"arifle_mas_mp40_o", 		// open stock
+	//MP5
+	"arifle_mas_mp5",			// stock
+	"arifle_mas_mp5_v",			// camo
+	"arifle_mas_mp5_d",			// desert
+	"arifle_mas_mp5sd",			// MP5SD6
+	"arifle_mas_mp5sd_ds",		// MP5SD6 Scuba
+	//Bizon
+	"arifle_mas_bizon", 		// stock
+	//MP7A1 RIFLE SLOT
+	"hgun_mas_mp7_F"			// stock
+		};
+	};
+
+	class MASLightMachineGuns 
+	{
+		name = "MAS Light Machine Guns";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] = 
+		{
+	//Stoner LMG (mk200)
+	"LMG_mas_MK200_F",			// stock
+	//Mk48
+	"LMG_mas_Mk48_F",			// stock
+	"LMG_mas_Mk48_F_v",			// camo
+	"LMG_mas_Mk48_F_d",			// desert
+	//M240
+	"LMG_mas_m240_F",			// stock
+	//MG3
+	"LMG_mas_mg3_F",			// Stock
+	//M60
+	"LMG_mas_m60_F",			// stock
+	//Zastava M72
+	"LMG_mas_m72_F",			// stock
+	//RPK
+	"LMG_mas_rpk_F",			// stock
+	//PKM
+	"LMG_mas_pkm_F",			// stock
+	//PKP
+	"LMG_mas_pech_F"			// stock	
+		};
+	};
+
+	class MASAssaultRifles
+	{
+		name = "MAS Assault Rifles";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] = 
+		{
+	//M16
+	"arifle_mas_m16", 			// stock a4
+	"arifle_mas_m16a2", 		// stock a2
+	"arifle_mas_m16_gl", 		// m203 a4
+	"arifle_mas_m16a2_gl", 		// m203 a2
+	//G36c
+	"arifle_mas_g36c", 			// stock
+	//M4
+	"arifle_mas_m4", 			// stock
+	"arifle_mas_m4_gl",			// gl
+	"arifle_mas_m4_m203",		// m203
+	"arifle_mas_m4_v",			// camo
+	"arifle_mas_m4_gl_v",		// gl camo
+	"arifle_mas_m4_m203_v",		// m203 camo
+	"arifle_mas_m4_d", 			// desert
+	"arifle_mas_m4_gl_d",		// gl desert
+	"arifle_mas_m4_m203_d", 	// m203 desert
+	"arifle_mas_m4vlt",			// VLTOR
+	"arifle_mas_m4c", 			// cqb stock
+	"arifle_mas_m4_m203c",		// cqb m203
+	"arifle_mas_m4c_v", 		// cqb camo
+	"arifle_mas_m4_m203c_v", 	// cqb m203 camo
+	"arifle_mas_m4c_d",			// cqb desert
+	"arifle_mas_m4_m203c_d",	// cqb m203 desert
+	//Hk 416
+	"arifle_mas_hk416", 		// stock
+	"arifle_mas_hk416_gl", 		// gl (fires further)
+	"arifle_mas_hk416_m203", 	// m203 (shorter range than gl)
+	"arifle_mas_hk416_v", 		// camo
+	"arifle_mas_hk416_gl_v", 	// camo gl (fires further)
+	"arifle_mas_hk416_m203_v", 	// camo m203
+	"arifle_mas_hk416_d", 		// desert (shorter range than gl)
+	"arifle_mas_hk416_gl_d", 	// desert gl (fires further)
+	"arifle_mas_hk416_m203_d", 	// desert m203 (shorter range than gl)
+	"arifle_mas_hk416c", 		// cqb stock
+	"arifle_mas_hk416_m203c", 	// cqb m203
+	"arifle_mas_hk416c_v", 		// cqb camo
+	"arifle_mas_hk416_m203c_v", // cqb camo m203
+	"arifle_mas_hk416c_d", 		// cqb desert
+	"arifle_mas_hk416_m203c_d", // cqb desert m203
+	//ARX160
+	"arifle_mas_arx", 			// stock
+	"arifle_mas_arx_gl",		// gl
+	"arifle_mas_arx_l", 		// black long barrel
+	"arifle_mas_arx_l_gl",		// black long barrel gl
+	//L119A1
+	"arifle_mas_l119", 			// stock
+	"arifle_mas_l119c", 		// cqb
+	"arifle_mas_l119_gl", 		// gl
+	"arifle_mas_l119_m203", 	// m203
+	"arifle_mas_l119_v", 		// camo
+	"arifle_mas_l119c_v", 		// cqb camo
+	"arifle_mas_l119_gl_v", 	// gl camo
+	"arifle_mas_l119_m203_v", 	// m203 camo
+	"arifle_mas_l119_d", 		// desert
+	"arifle_mas_l119c_d", 		// cqb desert
+	"arifle_mas_l119_gl_d", 	// gl desert
+	"arifle_mas_l119_m203_d", 	// m203 desert
+	//SCAR-LS (Short barrel)
+	"arifle_mas_mk16", 			// short barrel
+	"arifle_mas_mk16_gl", 		// short barrel gl
+	//SCAR-L
+	"arifle_mas_mk16_l", 		// stock
+	"arifle_mas_mk16_l_gl", 	// gl
+	//M27 IAR
+	"arifle_mas_m27", 			// stock
+	"arifle_mas_m27m", 			// supports drum mag
+	"arifle_mas_m27_v", 		// camo
+	"arifle_mas_m27m_v", 		// Drum camo
+	"arifle_mas_m27_d", 		// desert
+	"arifle_mas_m27m_d", 		// desert drum
+	//AK74M
+	"arifle_mas_ak_74m",		// stock
+	"arifle_mas_ak_74m_gl",		// gl
+	"arifle_mas_ak_74m_c",		// camo
+	"arifle_mas_ak_74m_gl_c",	// camo gl
+	"arifle_mas_ak_74m_sf",		// sf (rails)
+	"arifle_mas_ak_74m_sf_gl",	// sf gl (rails)
+	"arifle_mas_ak_74m_sf_c",	// sf camo (rails)
+	"arifle_mas_ak_74m_sf_gl_c",// sf gl camo (rails)
+	//AKS74
+	"arifle_mas_aks74",			// stock
+	"arifle_mas_aks74_gl",		// gl
+	"arifle_mas_aks74u",		// 74u
+	"arifle_mas_aks74u_c",		// 74u camo
+	//AK74
+	"arifle_mas_ak74",			// stock
+	"arifle_mas_ak74_gl",		// gl
+	//AK12
+	"arifle_mas_ak12_sf",		// stock
+	"arifle_mas_ak12_sf_gl",	// gl
+	//AKMS
+	"arifle_mas_akms",			// stock
+	"arifle_mas_akms_gl",		// gl
+	"arifle_mas_akms_c",		// camo
+	"arifle_mas_akms_gl_c",		// gl camo
+	//AKM
+	"arifle_mas_akm",			// stock
+	"arifle_mas_akm_gl",		// gl
+	//Zastava M70
+	"arifle_mas_m70",			// stock
+	"arifle_mas_m70_gl",		// gl
+	"arifle_mas_m70ab",			// folding stock
+	"arifle_mas_m70ab_gl",		// folding stock gl
+	//Hk 417
+	"arifle_mas_hk417c", 		// cqb
+	"arifle_mas_hk417_m203c", 	// cqb m203
+	"arifle_mas_hk417c_v", 		// cqb camo
+	"arifle_mas_hk417_m203c_v", // cqb m203 camo
+	"arifle_mas_hk417c_d", 		// cqb desert
+	"arifle_mas_hk417_m203c_d", // cqb m203 desert
+	"srifle_mas_hk417", 		// stock
+	"srifle_mas_hk417_v", 		// camo
+	"srifle_mas_hk417_d", 		// desert
+	//SCAR-H
+	"arifle_mas_mk17", 			// stock
+	"arifle_mas_mk17_gl", 		// gl
+	//G3
+	"arifle_mas_g3", 			// stock
+	"arifle_mas_g3_m203", 		// m203
+	"arifle_mas_g3s", 			// folding stock
+	"arifle_mas_g3s_m203", 		// folding stock m203
+	//FN FAL
+	"arifle_mas_fal", 			// stock
+	"arifle_mas_fal_m203", 		// m203
+	//M14 Old
+	"arifle_mas_m14", 			// stock
+	//PPSh-41
+	"arifle_mas_ppsh"			// stock
+		};
+	};
+
+	class MASSniperRifles
+	{
+		name = "MAS Sniper Rifles";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] = 
+		{	
+	//Lee-Enfield No4
+	"arifle_mas_lee", 			// stock
+	//VSS Vintorez
+	"srifle_mas_vss",			// stock
+	//AS VAL
+	"arifle_mas_asval",			// stock
+	"arifle_mas_asval_ds",		// underwater
+	//M14 EBR
+	"srifle_mas_ebr",			// stock
+	//SR25
+	"srifle_mas_sr25", 			// stock
+	"srifle_mas_sr25_v", 		// camo
+	"srifle_mas_sr25_d", 		// desert
+	//M110
+	"srifle_mas_m110", 			// stock
+	//SCAR-H Mark
+	"srifle_mas_mk17s", 		// stock
+	//M24
+	"srifle_mas_m24", 			// stock
+	"srifle_mas_m24_v",			// camo
+	"srifle_mas_m24_d",			// desert
+	//SVD
+	"srifle_mas_svd",			// stock
+	//Zastava M91
+	"srifle_mas_m91",			// stock
+	//KSVK
+	"srifle_mas_ksvk",			// stock
+	"srifle_mas_ksvk_c",		// camo
+	//LRR AWM
+	"srifle_mas_lrr",			// stock
+	//M107
+	"srifle_mas_m107",			// stock
+	"srifle_mas_m107_v",		// camo
+	"srifle_mas_m107_d"		// desert
+		};
+	};
+
+	
+	class MASLaunchers 
+	{
+		name = "MAS Launchers";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] = 
+		{
+	"mas_launch_RPG7_F",
+	"mas_launch_RPG18_F", 
+	"mas_launch_Stinger_F"
+		};
+	};	
+	
+	class MASLauncherAmmo 
+	{
+		name = "MAS Launcher Ammo";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] = 
+		{
+	"mas_PG18",
+	"mas_PG7V", 
+	"mas_OG7", 
+	"mas_PG7L", 
+	"mas_PG7VR", 
+	"mas_TBG7V",
+	"mas_Stinger"
+		};
+	};		
+
+
+	class MASBackpacks 
+	{
+		name = "MAS Backpacks";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\backpack_ca.paa";
+		items[] = 
+		{
+
+		};
+	};		
