4de6ef9dcb8cde16d72eb12e1dcfd66eca6affdb
thirdhero <ahmed-banna@hotmail.com>
2016-06-28 15:27:13 -0700

    done

--- GG/config.cpp
+++ GG/config.cpp
@@ -5885,7 +5885,7 @@ class CfgTrading
 	* items and vehicles. It is used if there is no sales price defined
 	* in CfgExileArsenal. 
 	*/
-	sellPriceFactor = 0.5;
+	sellPriceFactor = 0.25;
 	
 	rekeyPriceFactor = 0.1;
 };
