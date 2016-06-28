32e8abe511f37fe123b603a8c55146e679c338e0
thirdhero <ahmed-banna@hotmail.com>
2016-06-28 00:37:13 -0700

    Revert "Revert "fuck u nasty""
    This reverts commit ff3d3332e281303f50139469c3a70e0fc9756049.

--- GG/cfg/MAS/ItemListMAS.hpp
+++ GG/cfg/MAS/ItemListMAS.hpp
@@ -138,7 +138,6 @@
 	///////////////////////////////////////////////////////////////////////////////
 	// Pistols
 	///////////////////////////////////////////////////////////////////////////////
-    class hgun_mas_mak_F 							{ quality = 1; price = 600; };
     class hgun_mas_uzi_F 							{ quality = 1; price = 1500; };
     class hgun_mas_m9_F 							{ quality = 1; price = 600; };
 	class hgun_mas_acp_F							{ quality = 1; price = 700; };
@@ -146,15 +145,7 @@
     class hgun_mas_glock_F 							{ quality = 1; price = 600; };
     class hgun_mas_p226_F 							{ quality = 1; price = 600; };
     class hgun_mas_grach_F 							{ quality = 1; price = 600; };
-	class hgun_mas_acp_F							{ quality = 1; price = 800; sellPrice = 160; };
-	class hgun_mas_usp_F							{ quality = 1; price = 800; sellPrice = 160; };
     class hgun_mas_mak_F 							{ quality = 1; price = 700; };
-    class hgun_mas_uzi_F 							{ quality = 1; price = 1500; sellPrice = 300; };
-    class hgun_mas_m9_F 							{ quality = 1; price = 800; };
-    class hgun_mas_bhp_F 							{ quality = 1; price = 800; sellPrice = 160; };
-    class hgun_mas_glock_F 							{ quality = 1; price = 925; };
-    class hgun_mas_p226_F 							{ quality = 1; price = 800; sellPrice = 160; };
-    class hgun_mas_grach_F 							{ quality = 1; price = 800; };
     class hgun_mas_sa61_F 							{ quality = 1; price = 1500; };
     class hgun_mas_mp7p_F 							{ quality = 1; price = 3500; };
 	class hgun_mas_glocksf_F						{ quality = 1; price = 600; };
@@ -181,20 +172,15 @@
     class arifle_mas_bizon							{ quality = 1; price = 3500; };
     class hgun_mas_mp7_F							{ quality = 1; price = 3500; };
 	class arifle_mas_sten							{ quality = 1; price = 2500; };
-    class arifle_mas_mp40_o 						{ quality = 1; price = 2500; sellPrice = 500; };
-    class arifle_mas_mp5 							{ quality = 1; price = 2500; sellPrice = 500; };
     class arifle_mas_mp5_v 							{ quality = 1; price = 2500; };
     class arifle_mas_mp5_d							{ quality = 1; price = 2500; };
     class arifle_mas_mp5sd 							{ quality = 1; price = 3000; };
     class arifle_mas_mp5sd_ds 						{ quality = 1; price = 2500; };
-    class arifle_mas_bizon							{ quality = 1; price = 3000; sellPrice = 600; };
-    class hgun_mas_mp7_F							{ quality = 1; price = 4000; sellPrice = 800; };
 
 	///////////////////////////////////////////////////////////////////////////////
 	// Light Machine Guns
 	///////////////////////////////////////////////////////////////////////////////
     class LMG_mas_MK200_F							{ quality = 1; price = 8000; };
-    class LMG_mas_M249_F_v							{ quality = 1; price = 8000; sellPrice = 1600; };
     class LMG_mas_Mk48_F 							{ quality = 1; price = 9500; };
     class LMG_mas_Mk48_F_v 							{ quality = 1; price = 9500; sellPrice = 1900; };
     class LMG_mas_Mk48_F_d							{ quality = 1; price = 9500; };
@@ -217,10 +203,6 @@
     class arifle_mas_m16a2 							{ quality = 1; price = 5000; };
     class arifle_mas_m16_gl 						{ quality = 1; price = 5500; };
     class arifle_mas_m16a2_gl 						{ quality = 1; price = 5500; };
-    class arifle_mas_m16 							{ quality = 1; price = 6000; sellPrice = 1200; };
-    class arifle_mas_m16a2 							{ quality = 1; price = 6000; };
-    class arifle_mas_m16_gl 						{ quality = 1; price = 6000; };
-    class arifle_mas_m16a2_gl 						{ quality = 1; price = 6000; };
     class arifle_mas_g36c 							{ quality = 1; price = 6000; };
     class arifle_mas_m4 							{ quality = 1; price = 6000; };
     class arifle_mas_m4_gl							{ quality = 1; price = 6500; };
@@ -232,9 +214,6 @@
     class arifle_mas_m4_gl_d						{ quality = 1; price = 6500; };
     class arifle_mas_m4_m203_d 						{ quality = 1; price = 6500; };
     class arifle_mas_m4vlt							{ quality = 1; price = 6000; };
-    class arifle_mas_m4_gl_d						{ quality = 1; price = 8000; };
-    class arifle_mas_m4_m203_d 						{ quality = 1; price = 7000; };
-    class arifle_mas_m4vlt							{ quality = 1; price = 6000; sellPrice = 1200; };
     class arifle_mas_m4c 							{ quality = 1; price = 6000; };
     class arifle_mas_m4_m203c 						{ quality = 1; price = 6500; };
     class arifle_mas_m4c_v 							{ quality = 1; price = 6000; };
@@ -254,9 +233,6 @@
     class arifle_mas_hk416_m203c 					{ quality = 1; price = 6500; };
     class arifle_mas_hk416c_v 						{ quality = 1; price = 6000; };
     class arifle_mas_hk416_m203c_v 					{ quality = 1; price = 6500; };
-    class arifle_mas_hk416_m203c 					{ quality = 1; price = 7000; };
-    class arifle_mas_hk416c_v 						{ quality = 1; price = 6000; sellPrice = 1200; };
-    class arifle_mas_hk416_m203c_v 					{ quality = 1; price = 7000; };
     class arifle_mas_hk416c_d 						{ quality = 1; price = 6000; };
     class arifle_mas_hk416_m203c_d 					{ quality = 1; price = 6500; };
     class arifle_mas_arx 							{ quality = 1; price = 6000; };
@@ -330,9 +306,6 @@
     class arifle_mas_fal_m203						{ quality = 1; price = 8500; };
     class arifle_mas_m14 							{ quality = 1; price = 9000; };
     class arifle_mas_ppsh 							{ quality = 1; price = 3000; };
-    class arifle_mas_fal_m203						{ quality = 1; price = 9500; sellPrice = 1900; };
-    class arifle_mas_m14 							{ quality = 1; price = 8000; };
-    class arifle_mas_ppsh 							{ quality = 1; price = 8000; };
 	
 	///////////////////////////////////////////////////////////////////////////////
 	// Sniper Rifles
@@ -346,10 +319,6 @@
     class srifle_mas_sr25_v 						{ quality = 1; price = 12000; };
     class srifle_mas_sr25_d 						{ quality = 1; price = 12000; };
     class srifle_mas_m110 							{ quality = 1; price = 12000; };
-    class srifle_mas_sr25 							{ quality = 1; price = 10000; };
-    class srifle_mas_sr25_v 						{ quality = 1; price = 10000; };
-    class srifle_mas_sr25_d 						{ quality = 1; price = 10000; };
-    class srifle_mas_m110 							{ quality = 1; price = 10000; sellPrice = 2000; };
     class srifle_mas_mk17s 							{ quality = 1; price = 12000; };
     class srifle_mas_m24 							{ quality = 1; price = 8000; };
     class srifle_mas_m24_v							{ quality = 1; price = 8000; };
