c3cbd61f0e3b9daa77400470bb56e920f4fb23fe
thirdhero <ahmed-banna@hotmail.com>
2016-06-29 20:29:19 -0700

    hue

--- GG/config.cpp
+++ GG/config.cpp
@@ -4101,52 +4101,6 @@ class CfgInteractionMenus
 		};
 	};
 
-
-	class Safe
-	{
-		targetType = 2;
-		target = "Exile_Container_Safe";
-
-		class Actions 
-		{
-			class ScanLock: ExileAbstractAction
-			{
-				title = "Scan Lock";
-				condition = "('Exile_Item_ThermalScannerPro' in (magazines player)) && !((ExileClientInteractionObject getvariable ['ExileIsLocked',1]) isEqualTo 1) && !ExilePlayerInSafezone";
-				action = "_this call ExileClient_object_lock_scan";
-			};
-
-			// Locks a vehicle
-			class Lock : ExileAbstractAction
-			{
-				title = "Lock";
-				condition = "((ExileClientInteractionObject getvariable ['ExileIsLocked',1]) isEqualTo 0)";
-				action = "true spawn ExileClient_object_lock_toggle";
-			};
-
-			class Unlock : ExileAbstractAction
-			{
-				title = "Unlock";
-				condition = "((ExileClientInteractionObject getvariable ['ExileIsLocked',1]) isEqualTo -1)";
-				action = "false spawn ExileClient_object_lock_toggle";
-			};
-
-			class Pack : ExileAbstractAction
-			{
-				title = "Pack";
-				condition = "((ExileClientInteractionObject getvariable ['ExileIsLocked',1]) isEqualTo 0)";
-				action = "_this spawn ExileClient_object_container_pack";
-			};
-
-			class SetPinCode : ExileAbstractAction
-			{
-				title = "Set PIN";
-				condition = "((ExileClientInteractionObject getvariable ['ExileIsLocked',1]) isEqualTo 0)";
-				action = "_this spawn ExileClient_object_lock_setPin";
-			};
-		};
-	};
-
 	class Laptop
 	{
 		targetType = 2;
