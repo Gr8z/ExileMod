21fb1d294ea84d3d5cd302810101237da5ea119f
GR8 <gr8_boi52@yahoo.com>
2016-07-13 11:30:55 +0400

    fix tanoa temp

--- GG/config.cpp
+++ GG/config.cpp
@@ -4064,6 +4064,111 @@ class CfgExileEnvironment
 			water = -5;
 		};
 	};
+
+	class Tanoa 
+	{
+		// A list of {position, radius} where building is not allowed at all
+		nonConstructionZones[] = {
+			{{8347.18,5990.92,291.991}, 1000};
+			};
+
+
+		class FireFlies
+		{
+			// 1 = enabled, 0 = disabled
+			enable = 0;
+
+			// At this hour fire flies begin to spawn
+			startHour = 18;
+
+			// At this hour fire flies stop spawning
+			endHour = 4;
+		};
+
+		class Anomalies
+		{
+			// 1 = enabled, 0 = disabled
+			enable = 0;
+
+			// At this hour anomalies begin to spawn
+			startHour = 19;
+
+			// At this hour anomalies stop spawning
+			endHour = 6;
+		};
+
+		class Breathing
+		{
+			// 1 = enabled, 0 = disabled
+			enable = 0;
+		};
+
+		class Snow
+		{
+			// 1 = enabled, 0 = disabled
+			enable = 0;
+
+			// https://community.bistudio.com/wiki/surfaceType
+			surfaces[] = {};
+		};
+
+		class Radiation 
+		{
+			// 1 = enabled, 0 = disabled
+			enable = 1;
+
+			/*
+				Defines contaminated zones in a specific map. 
+				You can define multiple zones per map. The format
+				of the zones is:
+
+				[Position ASL(!), Full Radiation Radius, Maximum Radius]
+
+				The radius works as follows:
+
+	            |-------------------------------------------------------|
+	                                Maximum Radius
+				
+				|------------------------|
+				  Full Radiation Radius   
+
+				Within the full radiation radius, radiation factor is
+				always at a maximum. Outside of this, it lowers down
+				to no radiation smoothly.
+
+				Radiation:
+
+	            |------------------------|------------------------------|
+	            1        1      1        1     0.75    0.5     0.25    0
+			*/
+			contaminatedZones[] = 
+			{
+				{{8347.18,5990.92,291.991}, 300, 400}	// radiation church
+			};
+		};
+
+		class Temperature
+		{
+			// Temperature in °C for the time of day, per hour
+			// Add the first index to the last index, so it is 25 indizes!
+			daytimeTemperature[] = {15.93,16.89,18.42,20.40,22.68,25.10,27.48,29.63,31.40,32.66,33.32,33.80,33.80,33.32,32.66,31.40,29.63,27.48,25.10,22.68,20.40,18.42,16.89,15.93,15.93};
+		
+			// Temperature change in °C when it is 100% overcast
+			overcast = -2;
+
+			// Temperature change in °C when it is 100% raining
+			rain = -5;
+
+			// Temperature change in °C when it is 100% windy
+			wind = -1;
+
+			// Temperature change per 100m altitude in °C
+			altitude = -0.5;
+
+			// Difference from the daytime temperature to the water temperature
+			water = -5;
+		};
+	};
 };
 class CfgExileHUD
 {
