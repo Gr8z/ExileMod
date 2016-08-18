0756c6b9beaf1fc3fe9d85c5d03c1e5937d9c83c
thirdhero <ahmed-banna@hotmail.com>
2016-08-17 18:40:35 -0700

    Revert "Revert "Radiation Zone 2""
    This reverts commit 449c6855994a1f94cd4f5fe66274532c2282e454.

--- GG/config.cpp
+++ GG/config.cpp
@@ -4012,7 +4012,7 @@ class CfgExileEnvironment
 	{
 		// A list of {position, radius} where building is not allowed at all
 		nonConstructionZones[] = {
-			{{8347.18,5990.92,291.991}, 1000};
+			{{7562.91,0,5149.56}, 1000};
 			};
 
 
@@ -4086,7 +4086,7 @@ class CfgExileEnvironment
 			*/
 			contaminatedZones[] = 
 			{
-				{{8347.18,5990.92,291.991}, 300, 400}	// radiation church
+				{{7562.91,0,5149.56}, 300, 400}	// radiation mogilevka
 			};
 		};
 
