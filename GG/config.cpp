4de5b21b8d95246f0ce83c691876ade7457ccd13
ShroomBear <kzeka123@aol.com>
2016-08-21 22:49:21 -0400

    Qilin price increase 75k

--- GG/config.cpp
+++ GG/config.cpp
@@ -2798,10 +2798,10 @@ class CfgExileArsenal
 	// Qilin
 	///////////////////////////////////////////////////////////////////////////////
 	class Exile_Car_QilinUnarmed						{ quality = 2; price = 20000; };
-	class O_lsv_02_armed_f						 		{ quality = 2; price = 60000; };
-	class O_LSV_02_armed_black_f						{ quality = 2; price = 60000; };
-	class O_LSV_02_armed_arid_f							{ quality = 2; price = 60000; };
-	class O_LSV_02_armed_ghex_f							{ quality = 2; price = 60000; };
+	class O_lsv_02_armed_f						 		{ quality = 2; price = 75000; };
+	class O_LSV_02_armed_black_f						{ quality = 2; price = 75000; };
+	class O_LSV_02_armed_arid_f							{ quality = 2; price = 75000; };
+	class O_LSV_02_armed_ghex_f							{ quality = 2; price = 75000; };
 	///////////////////////////////////////////////////////////////////////////////
 	// MB 4WD
 	///////////////////////////////////////////////////////////////////////////////
