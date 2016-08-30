bbb60ac1b87af1d5780a94c43e11710c055e677c
thirdhero <ahmed-banna@hotmail.com>
2016-08-30 03:30:12 -0700

    Revert "test CfgExileCustomCode with class"
    This reverts commit ea2897e457a6628460f1fb1959d75a560246180f.

--- GG/config.cpp
+++ GG/config.cpp
@@ -2809,8 +2809,16 @@ class CfgExileArsenal
 	class Exile_Car_TowTractor_White									{ quality = 1; price = 1800; };
 };
 
-#include custom\CfgExileCustomCode.hpp
 
+
+class CfgExileCustomCode 
+{
+	ExileClient_gui_xm8_slide = 			"GG\custom\ExileClient_gui_xm8_slide.sqf";
+	ExileClient_gui_xm8_show = 				"GG\custom\ExileClient_gui_xm8_show.sqf";
+	ExileClient_construction_handleAbort = 	"GG\custom\ExileClient_construction_handleAbort.sqf";
+	ExileClient_object_item_construct = 	"GG\custom\ExileClient_object_item_construct.sqf";
+	ExileClient_construction_thread = 		"GG\custom\ExileClient_construction_thread.sqf";
+};
 class CfgExileEnvironment
 {
 	class Altis 
