ad2c3231bdf402737497c150da7ebe6fe00b66b7
thirdhero <ahmed-banna@hotmail.com>
2016-08-30 03:05:05 -0700

    test

--- GG/config.cpp
+++ GG/config.cpp
@@ -2813,7 +2813,11 @@ class CfgExileArsenal
 
 class CfgExileCustomCode 
 {
-	#include custom\CfgExileCustomCode.hpp
+	ExileClient_gui_xm8_slide = 			"GG\custom\ExileClient_gui_xm8_slide.sqf";
+	ExileClient_gui_xm8_show = 				"GG\custom\ExileClient_gui_xm8_show.sqf";
+	ExileClient_construction_handleAbort = 	"GG\custom\ExileClient_construction_handleAbort.sqf";
+	ExileClient_object_item_construct = 	"GG\custom\ExileClient_object_item_construct.sqf";
+	ExileClient_construction_thread = 		"GG\custom\ExileClient_construction_thread.sqf";
 };
 class CfgExileEnvironment
 {
