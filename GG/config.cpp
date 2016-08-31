6305efe35159e362343529b49d9b5fe3b85d8284
GR8 <gr8_boi52@yahoo.com>
2016-08-31 18:09:02 +0500

    missing semi colon

--- GG/config.cpp
+++ GG/config.cpp
@@ -2812,7 +2812,7 @@ class CfgExileCustomCode
     ExileClient_object_player_event_onEnterSafezone =       							"GG\custom\ExileClient_object_player_event_onEnterSafezone.sqf"; 
     ExileClient_object_player_event_onLeaveSafezone =       							"GG\custom\ExileClient_object_player_event_onLeaveSafezone.sqf";
     ExileClient_gui_wasteDumpDialog_show =                  							"GG\custom\ExileClient_gui_wasteDumpDialog_show.sqf";
-    ExeClient_object_player_event_onInventoryOpened =     								"GG\custom\ExileClient_object_player_event_onInventoryOpened.sqf"
+    ExeClient_object_player_event_onInventoryOpened =     								"GG\custom\ExileClient_object_player_event_onInventoryOpened.sqf";
     ExileClient_gui_upgradeTerritoryDialog_event_onTerritoryDropDownSelectionChanged = 	"GG\custom\ExileClient_gui_upgradeTerritoryDialog_event_onTerritoryDropDownSelectionChanged.sqf";
 };
 class CfgExileEnvironment
