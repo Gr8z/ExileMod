d24906d2aad7b600c717a176a6064769494c9388
GR8 <gr8_boi52@yahoo.com>
2016-09-05 21:29:06 +0500

    AVS

--- GG/config.cpp
+++ GG/config.cpp
@@ -10,6 +10,10 @@ class CfgRemoteExec
 		class fnc_AdminReq { allowedTargets=2; };
 		class ExileServer_system_network_dispatchIncomingMessage { allowedTargets=2; };
 		class ExAdServer_fnc_clientRequest { allowedTargets=2; };
+		class AVS_fnc_rearmVehicle									{ allowedTargets=2; };
+        class AVS_fnc_refuelPayment                                 { allowedTargets=2; };
+        class AVS_fnc_refuelVehicle									{ allowedTargets=2; };
+		class AVS_fnc_fillCanister									{ allowedTargets=2; };
 	};
 	class Commands
 	{
--- maps/Tanoa/init.sqf
+++ maps/Tanoa/init.sqf
@@ -1,1 +1,2 @@
 [] execVM "GG\battlezones.sqf";
+[] execVM "GG\AVS\AVS_fnc_init.sqf";
