6aa4b456272132d136bf5ab63b480722eb2f1edf
GR8 <gr8_boi52@yahoo.com>
2016-09-05 22:29:00 +0500

    battlezone notification

--- GG/config.cpp
+++ GG/config.cpp
@@ -22,6 +22,65 @@ class CfgRemoteExec
 	};
 };	
 
+class CfgNotifications
+{
+  class Success
+  {
+      description = "%1";
+      title = "Success";
+      iconPicture = "\A3\ui_f\data\map\mapcontrol\taskIcon_ca.paa";
+      iconText = "";
+      color[] = {0.01,0.61,0.94,1};
+      duration = 5;
+      priority = 0;
+      difficulty[] = {};
+  };
+  class Whoops
+  {
+      description = "%1";
+      title = "Error";
+      iconPicture = "\A3\ui_f\data\map\mapcontrol\taskIcon_ca.paa";
+      iconText = "";
+      color[] = {0.01,0.61,0.94,1};
+      duration = 5;
+      priority = 0;
+      difficulty[] = {};
+  };
+	class ZCP_Init
+	{
+		description = "%1";
+		title = "Battle Zone Spawned";
+		iconPicture = "\A3\ui_f\data\map\mapcontrol\taskIcon_ca.paa";
+		iconText = "";
+		color[] = {0.01,0.61,0.94,1};
+		duration = 5;
+		priority = 0;
+		difficulty[] = {};
+	};
+	class ZCP_Capped
+	{
+		description = "%1";
+		title = "Battle Zone Capped";
+		iconPicture = "\A3\ui_f\data\map\mapcontrol\taskIcon_ca.paa";
+		iconText = "";
+		color[] = {0.01,0.61,0.94,1};
+		duration = 5;
+		priority = 0;
+		difficulty[] = {};
+	};
+	class ZCP_Capping
+	{
+		description = "%1";
+		title = "Battle Zone alert";
+		iconPicture = "\A3\ui_f\data\map\mapcontrol\taskIcon_ca.paa";
+		iconText = "";
+		color[] = {0.01,0.61,0.94,1};
+		duration = 5;
+		priority = 0;
+		difficulty[] = {};
+	};
+};
+
 class CfgClans
 {
 	/*
