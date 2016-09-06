79d78b3b45025991bae29388ce5fd570de0c7680
ShroomBear <kzeka123@aol.com>
2016-09-05 22:45:01 -0400

    Revert "Revert "Revert "update bat file"""
    This reverts commit b2dd0458e0b420736a87786ffd0738f1bf526957.

--- configs/exile_1/updatePBOs.bat
+++ configs/exile_1/updatePBOs.bat
@@ -17,7 +17,6 @@
 "C:\Program Files\PBO Manager v.1.4 beta\PBOConsole.exe" -pack "C:\ExileMod\addons\a3_exile_occupation" "D:\a3Server\@ExileServer\addons\a3_exile_occupation.pbo"
 "C:\Program Files\PBO Manager v.1.4 beta\PBOConsole.exe" -pack "C:\ExileMod\addons\XG_killboard" "D:\a3Server\@ExileServer\addons\XG_killboard.pbo"
 "C:\Program Files\PBO Manager v.1.4 beta\PBOConsole.exe" -pack "C:\ExileMod\addons\AVS" "D:\a3Server\@ExileServer\addons\AVS.pbo"
-"C:\Program Files\PBO Manager v.1.4 beta\PBOConsole.exe" -pack "C:\ExileMod\addons\HEG_Xbrm" "D:\a3Server\@ExileServer\addons\HEG_Xbrm.pbo"
 
 echo Creating new PBOs 
 
