eacf8b0726f72ed90cc93c6bd5164fb3994e0a90
GR8 <gr8_boi52@yahoo.com>
2016-09-07 13:47:18 +0500

    Added Extension cord to spawn

--- addons/exile_server_config/CfgSettings.hpp
+++ addons/exile_server_config/CfgSettings.hpp
@@ -198,7 +198,8 @@ class CfgSettings
 			"ItemMap", // Because why not
 			"Exile_Item_XM8",
 			"ItemRadio",
-			"Exile_Item_PlasticBottleFreshWater"
+			"Exile_Item_PlasticBottleFreshWater",
+			"Exile_Item_ExtensionCord"
 		};
 
 		/**
