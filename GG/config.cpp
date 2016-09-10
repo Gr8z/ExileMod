a939240caa64f2aa4776a395e1d229cef09922ff
GR8 <gr8_boi52@yahoo.com>
2016-09-10 21:31:17 +0500

    New code XM8

--- GG/config.cpp
+++ GG/config.cpp
@@ -3936,7 +3936,7 @@ class CfgExileMobileXM8
 	 * Add the 4 digit permission code here, so players can authorize
 	 * your server to send them messages to their Mobile XM8 app.
 	 */
-	code = "3XYL";
+	code = "T5OD";
 };
 class CfgExileMusic
 {
