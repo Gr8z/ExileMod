86d4e95294061dc4f0257ddeccdf4a687cdc0e29
GR8 <gr8_boi52@yahoo.com>
2016-09-11 22:21:37 +0500

    set this to this becuase the parachute kills you

--- addons/exile_server_config/CfgSettings.hpp
+++ addons/exile_server_config/CfgSettings.hpp
@@ -222,12 +222,12 @@ class CfgSettings
 		 * 1 = On
 		 * 0 = Off
 		 */
-		haloJump = 1;
+		haloJump = 0;
 
 		/**
 		 * Parachute drop height in meters. 
 		 */
-		parachuteDropHeight = 600;
+		parachuteDropHeight = 200;
 
 		/**
 		 * Number of minutes where a fresh spawned player remains in the
