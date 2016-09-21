a7ca43a372ca9955ad3edd391220fb1777875647
GR8 <gr8_boi52@yahoo.com>
2016-09-21 17:52:01 +0500

    add shortcut button

--- GG/DailyReward/dialog/RscRewardDefines.hpp
+++ GG/DailyReward/dialog/RscRewardDefines.hpp
@@ -82,6 +82,122 @@ class RscRewardPicture
 	tooltipColorBox[] ={1,1,1,1};
 	tooltipColorShade[] ={0,0,0,0.65};
 };
+class RscRewardShortcutButton
+{
+	deletable = 0;
+	fade = 0;
+	type = 16;
+	x = 0.1;
+	y = 0.1;
+	class HitZone
+	{
+		left = 0;
+		top = 0;
+		right = 0;
+		bottom = 0;
+	};
+	class ShortcutPos
+	{
+		left = 0;
+		top = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 20) - (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)) / 2";
+		w = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1) * (3/4)";
+		h = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
+	};
+	class TextPos
+	{
+		left = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1) * (3/4)";
+		top = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 20) - (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)) / 2";
+		right = 0.005;
+		bottom = 0;
+	};
+	shortcuts[] =
+	{
+	};
+	textureNoShortcut = "#(argb,8,8,3)color(0,0,0,0)";
+	color[] ={1,1,1,1};
+	colorFocused[] ={1,1,1,1};
+	color2[] ={0.95,0.95,0.95,1};
+	colorDisabled[] ={1,1,1,0.25};
+	colorBackground[] =
+	{
+		"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.77])",
+		"(profilenamespace getvariable ['GUI_BCG_RGB_G',0.51])",
+		"(profilenamespace getvariable ['GUI_BCG_RGB_B',0.08])",
+		1
+	};
+	colorBackgroundFocused[] =
+	{
+		"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.77])",
+		"(profilenamespace getvariable ['GUI_BCG_RGB_G',0.51])",
+		"(profilenamespace getvariable ['GUI_BCG_RGB_B',0.08])",
+		1
+	};
+	colorBackground2[] ={1,1,1,1};
+	soundEnter[] =
+	{
+		"\A3\ui_f\data\sound\RscButton\soundEnter",
+		0.09,
+		1
+	};
+	soundPush[] =
+	{
+		"\A3\ui_f\data\sound\RscButton\soundPush",
+		0.09,
+		1
+	};
+	soundClick[] =
+	{
+		"\A3\ui_f\data\sound\RscButton\soundClick",
+		0.09,
+		1
+	};
+	soundEscape[] =
+	{
+		"\A3\ui_f\data\sound\RscButton\soundEscape",
+		0.09,
+		1
+	};
+	class Attributes
+	{
+		font = "PuristaMedium";
+		color = "#E5E5E5";
+		align = "left";
+		shadow = "true";
+	};
+	idc = -1;
+	style = 0;
+	default = 0;
+	shadow = 1;
+	w = 0.183825;
+	h = "((((safezoneW / safezoneH) min 1.2) / 1.2) / 20)";
+	textSecondary = "";
+	colorSecondary[] ={1,1,1,1};
+	colorFocusedSecondary[] ={1,1,1,1};
+	color2Secondary[] ={0.95,0.95,0.95,1};
+	colorDisabledSecondary[] ={1,1,1,0.25};
+	sizeExSecondary = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
+	fontSecondary = "PuristaMedium";
+	animTextureDefault = "\A3\ui_f\data\GUI\RscCommon\RscShortcutButton\normal_ca.paa";
+	animTextureNormal = "\A3\ui_f\data\GUI\RscCommon\RscShortcutButton\normal_ca.paa";
+	animTextureDisabled = "\A3\ui_f\data\GUI\RscCommon\RscShortcutButton\normal_ca.paa";
+	animTextureOver = "\A3\ui_f\data\GUI\RscCommon\RscShortcutButton\over_ca.paa";
+	animTextureFocused = "\A3\ui_f\data\GUI\RscCommon\RscShortcutButton\focus_ca.paa";
+	animTexturePressed = "\A3\ui_f\data\GUI\RscCommon\RscShortcutButton\down_ca.paa";
+	periodFocus = 1.2;
+	periodOver = 0.8;
+	period = 0.4;
+	font = "PuristaMedium";
+	size = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
+	sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
+	text = "";
+	action = "";
+	class AttributesImage
+	{
+		font = "PuristaMedium";
+		color = "#E5E5E5";
+		align = "left";
+	};
+};
 class RscRewardButton
 {
 	deletable = 0;
@@ -237,7 +353,7 @@ class RscRewardButtonMenu
 		1
 	};
 };
-class RscRewardButtonMenuOK : RscRewardButtonMenu
+class RscRewardButtonMenuOK : RscRewardShortcutButton
 {
 	idc = 1;
 	shortcuts[] ={
@@ -255,7 +371,7 @@ class RscRewardButtonMenuOK : RscRewardButtonMenu
 		1
 	};
 };
-class RscRewardButtonMenuCancel : RscRewardButtonMenu
+class RscRewardButtonMenuCancel : RscRewardShortcutButton
 {
 	idc = 2;
 	shortcuts[] =
