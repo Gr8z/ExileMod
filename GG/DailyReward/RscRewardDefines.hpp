e1262bfae3e630d8607e4a23102cb4f8c3fed41f
GR8 <gr8_boi52@yahoo.com>
2016-09-22 15:24:52 +0500

    remove useless data

--- GG/DailyReward/RscRewardDefines.hpp
+++ GG/DailyReward/RscRewardDefines.hpp
@@ -1,13 +1,10 @@
-#define GUI_GRID_X	(0)
-#define GUI_GRID_Y	(0)
-#define GUI_GRID_W	(0.025)
-#define GUI_GRID_H	(0.04)
+#define GUI_GRID_X		(0)
+#define GUI_GRID_Y		(0)
+#define GUI_GRID_W		(0.025)
+#define GUI_GRID_H		(0.04)
 #define GUI_GRID_WAbs	(1)
 #define GUI_GRID_HAbs	(1)
 
-///////////////////////////////////////////////////////////////////////////
-/// Base Classes
-///////////////////////////////////////////////////////////////////////////
 class RscRewardText
 {
 	deletable = 0;
@@ -81,122 +78,6 @@ class RscRewardPicture
 	tooltipColorText[] ={1,1,1,1};
 	tooltipColorBox[] ={1,1,1,1};
 	tooltipColorShade[] ={0,0,0,0.65};
-};
-class RscRewardShortcutButton
-{
-	deletable = 0;
-	fade = 0;
-	type = 16;
-	x = 0.1;
-	y = 0.1;
-	class HitZone
-	{
-		left = 0;
-		top = 0;
-		right = 0;
-		bottom = 0;
-	};
-	class ShortcutPos
-	{
-		left = 0;
-		top = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 20) - (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)) / 2";
-		w = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1) * (3/4)";
-		h = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
-	};
-	class TextPos
-	{
-		left = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1) * (3/4)";
-		top = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 20) - (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)) / 2";
-		right = 0.005;
-		bottom = 0;
-	};
-	shortcuts[] =
-	{
-	};
-	textureNoShortcut = "#(argb,8,8,3)color(0,0,0,0)";
-	color[] ={1,1,1,1};
-	colorFocused[] ={1,1,1,1};
-	color2[] ={0.95,0.95,0.95,1};
-	colorDisabled[] ={1,1,1,0.25};
-	colorBackground[] =
-	{
-		"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.77])",
-		"(profilenamespace getvariable ['GUI_BCG_RGB_G',0.51])",
-		"(profilenamespace getvariable ['GUI_BCG_RGB_B',0.08])",
-		1
-	};
-	colorBackgroundFocused[] =
-	{
-		"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.77])",
-		"(profilenamespace getvariable ['GUI_BCG_RGB_G',0.51])",
-		"(profilenamespace getvariable ['GUI_BCG_RGB_B',0.08])",
-		1
-	};
-	colorBackground2[] ={1,1,1,1};
-	soundEnter[] =
-	{
-		"\A3\ui_f\data\sound\RscButton\soundEnter",
-		0.09,
-		1
-	};
-	soundPush[] =
-	{
-		"\A3\ui_f\data\sound\RscButton\soundPush",
-		0.09,
-		1
-	};
-	soundClick[] =
-	{
-		"\A3\ui_f\data\sound\RscButton\soundClick",
-		0.09,
-		1
-	};
-	soundEscape[] =
-	{
-		"\A3\ui_f\data\sound\RscButton\soundEscape",
-		0.09,
-		1
-	};
-	class Attributes
-	{
-		font = "PuristaMedium";
-		color = "#E5E5E5";
-		align = "left";
-		shadow = "true";
-	};
-	idc = -1;
-	style = 0;
-	default = 0;
-	shadow = 1;
-	w = 0.183825;
-	h = "((((safezoneW / safezoneH) min 1.2) / 1.2) / 20)";
-	textSecondary = "";
-	colorSecondary[] ={1,1,1,1};
-	colorFocusedSecondary[] ={1,1,1,1};
-	color2Secondary[] ={0.95,0.95,0.95,1};
-	colorDisabledSecondary[] ={1,1,1,0.25};
-	sizeExSecondary = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
-	fontSecondary = "PuristaMedium";
-	animTextureDefault = "\A3\ui_f\data\GUI\RscCommon\RscShortcutButton\normal_ca.paa";
-	animTextureNormal = "\A3\ui_f\data\GUI\RscCommon\RscShortcutButton\normal_ca.paa";
-	animTextureDisabled = "\A3\ui_f\data\GUI\RscCommon\RscShortcutButton\normal_ca.paa";
-	animTextureOver = "\A3\ui_f\data\GUI\RscCommon\RscShortcutButton\over_ca.paa";
-	animTextureFocused = "\A3\ui_f\data\GUI\RscCommon\RscShortcutButton\focus_ca.paa";
-	animTexturePressed = "\A3\ui_f\data\GUI\RscCommon\RscShortcutButton\down_ca.paa";
-	periodFocus = 1.2;
-	periodOver = 0.8;
-	period = 0.4;
-	font = "PuristaMedium";
-	size = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
-	sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
-	text = "";
-	action = "";
-	class AttributesImage
-	{
-		font = "PuristaMedium";
-		color = "#E5E5E5";
-		align = "left";
-	};
 };
 class RscRewardButton
 {
@@ -274,92 +155,6 @@ class RscRewardFrame
 	y = 0;
 	w = 0.3;
 	h = 0.3;
-};
-class RscRewardButtonMenu
-{
-	idc = -1;
-	type = 16;
-	style = "0x02 + 0xC0";
-	default = 0;
-	shadow = 0;
-	x = 0;
-	y = 0;
-	w = 0.095589;
-	h = 0.039216;
-	animTextureNormal = "#(argb,8,8,3)color(1,1,1,1)";
-	animTextureDisabled = "#(argb,8,8,3)color(1,1,1,1)";
-	animTextureOver = "#(argb,8,8,3)color(1,1,1,1)";
-	animTextureFocused = "#(argb,8,8,3)color(1,1,1,1)";
-	animTexturePressed = "#(argb,8,8,3)color(1,1,1,1)";
-	animTextureDefault = "#(argb,8,8,3)color(1,1,1,1)";
-	colorBackground[] ={0,0,0,0.8};
-	colorBackgroundFocused[] ={1,1,1,1};
-	colorBackground2[] ={0.75,0.75,0.75,1};
-	color[] ={1,1,1,1};
-	colorFocused[] ={0,0,0,1};
-	color2[] ={0,0,0,1};
-	colorText[] ={1,1,1,1};
-	colorDisabled[] ={1,1,1,0.25};
-	textSecondary = "";
-	colorSecondary[] ={1,1,1,1};
-	colorFocusedSecondary[] ={0,0,0,1};
-	color2Secondary[] ={0,0,0,1};
-	colorDisabledSecondary[] ={1,1,1,0.25};
-	sizeExSecondary = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
-	fontSecondary = "PuristaLight";
-	period = 1.2;
-	periodFocus = 1.2;
-	periodOver = 1.2;
-	size = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
-	sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
-	tooltipColorText[] ={1,1,1,1};
-	tooltipColorBox[] ={1,1,1,1};
-	tooltipColorShade[] ={0,0,0,0.65};
-	class TextPos
-	{
-		left = "0.25 * (((safezoneW / safezoneH) min 1.2) / 40)";
-		top = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) - (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)) / 2";
-		right = 0.005;
-		bottom = 0;
-	};
-	class Attributes
-	{
-		font = "PuristaLight";
-		color = "#E5E5E5";
-		align = "left";
-		shadow = "false";
-	};
-	class ShortcutPos
-	{
-		left = "5.25 * (((safezoneW / safezoneH) min 1.2) / 40)";
-		top = 0;
-		w = "1 * (((safezoneW / safezoneH) min 1.2) / 40)";
-		h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-	};
-	soundEnter[] =
-	{
-		"\A3\ui_f\data\sound\RscButtonMenu\soundEnter",
-		0.09,
-		1
-	};
-	soundPush[] =
-	{
-		"\A3\ui_f\data\sound\RscButtonMenu\soundPush",
-		0.09,
-		1
-	};
-	soundClick[] =
-	{
-		"\A3\ui_f\data\sound\RscButtonMenu\soundClick",
-		0.09,
-		1
-	};
-	soundEscape[] =
-	{
-		"\A3\ui_f\data\sound\RscButtonMenu\soundEscape",
-		0.09,
-		1
-	};
 };
 class RscRewardButtonMenuOK : RscRewardButton
 {
