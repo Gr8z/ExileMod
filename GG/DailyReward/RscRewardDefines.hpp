6f960433b2f7bba03a46a7f1e37d3517d203fd86
GR8 <gr8_boi52@yahoo.com>
2016-09-22 15:25:31 +0500

    class RscRewardFrame removed

--- GG/DailyReward/RscRewardDefines.hpp
+++ GG/DailyReward/RscRewardDefines.hpp
@@ -140,22 +140,6 @@ class RscRewardButton
 	offsetPressedY = 0;
 	borderSize = 0;
 };
-class RscRewardFrame
-{
-	type = 0;
-	idc = -1;
-	style = 64;
-	shadow = 2;
-	colorBackground[] ={0,0,0,0};
-	colorText[] ={1,1,1,1};
-	font = "PuristaMedium";
-	sizeEx = 0.02;
-	text = "";
-	x = 0;
-	y = 0;
-	w = 0.3;
-	h = 0.3;
-};
 class RscRewardButtonMenuOK : RscRewardButton
 {
 	idc = 1;
