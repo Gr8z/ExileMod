489117cc42b6cb6fbed6a1cbe6046c901cc2950c
GR8 <gr8_boi52@yahoo.com>
2016-10-10 15:52:11 +0500

    3 new colors

--- GG/config/CfgFlags.hpp
+++ GG/config/CfgFlags.hpp
@@ -1,7 +1,22 @@
 class CfgFlags {
 	class USA {
-		name = USA;
+		name = "USA";
 		texture = "\A3\Data_F\Flags\flag_us_co.paa";
+		uids[] = {};
+	};
+	class Yellow {
+		name = "Yellow";
+		texture = "GG\images\colors\flag_yellow_co.paa";
+		uids[] = {};
+	};
+	class Purple {
+		name = "Purple";
+		texture = "GG\images\colors\flag_purple_co.paa";
+		uids[] = {};
+	};
+	class Orange {
+		name = "Orange";
+		texture = "GG\images\colors\flag_orange_co.paa";
 		uids[] = {};
 	};
 };
